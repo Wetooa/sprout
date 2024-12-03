e6b2590f5def33282c254102a9ca37923dd9b32e
wetooa <derpykidyt@gmail.com>
2024-12-03 16:04:27 +0800

    bug: made clear, may bug tho

--- frontend/lib/earth-engine.ts
+++ frontend/lib/earth-engine.ts
@@ -1,1 +1,32 @@
+import { Key } from "@/types/key";
+import ee from "@google/earthengine";
 
+export function earthEngineAuthenticate(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const key: Key = {
+      type: process.env.TYPE,
+      project_id: process.env.PROJECT_ID,
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      private_key: process.env.PRIVATE_KEY,
+      client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      auth_uri: process.env.AUTH_URI,
+      token_uri: process.env.TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+      universe_domain: process.env.UNIVERSE_DOMAIN,
+    };
+
+    ee.data.authenticateViaPrivateKey(
+      key,
+      () =>
+        ee.initialize(
+          null,
+          null,
+          () => resolve(),
+          (error: any) => reject(new Error(error)),
+        ),
+      (error: any) => reject(new Error(error)),
+    );
+  });
+}
